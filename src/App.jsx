271bef683443ee8a65638449f1a97174f854b5ec
Nishant Chakraborty <ncnishant333@gmail.com>
2026-04-25 15:10:53 +0530

    daisyui initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <h1 className="text-2xl font-bold">Test</h1>
+    </>
+  );
+}
+
+export default App;

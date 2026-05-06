c4c9a75c08ab67c6c6c540a34b6bd3d814fc4863
Nishant Chakraborty <ncnishant333@gmail.com>
2026-05-06 06:17:48 +0530

    auth state hydration done in the top level component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios, { HttpStatusCode } from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../reduxContent/userSlice";
 
 const Body = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try {
+      const user = await axios.get(BASE_URL + "profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(user.data));
+    } catch (error) {
+      if (error.response?.status === 401) {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Navbar />
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div> Profile</div>;
+  const selector = useSelector((store) => store.user);
+  return <div>{selector?.firstName}'s Profile</div>;
 };
 
 export default Profile;
